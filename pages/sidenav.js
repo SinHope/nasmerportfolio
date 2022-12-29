64606d26cd992b97087cce8081b1a325465a48a5
SinHope <nasmerfontanilla@gmail.com>
2022-12-30 00:59:41 +0800

    fixed sidebar nav issue

--- pages/sidenav.js
+++ pages/sidenav.js
@@ -1,26 +1,29 @@
 import React from "react";
-import styles from '../styles/Home.module.css';
+import { useState } from 'react';
+
+
 
 const SideNav = (props) => {
     return (
-<div className={styles.sidenav}>
-            <img src="\profilephoto.png" alt="Profile Photo" className="h-48 w-80 rounded-full mt-28 bg-white ml-[18px]"></img>
+<div className="w-[360px] fixed overflow-y-auto h-full bg-emerald-100 top-0 right-0 left-0">
+            <img src="\profilephoto.png" alt="Profile Photo" className="h-48 w-80 rounded-full mt-12 bg-white ml-[18px]"></img>
             <h1 className="font-quicksand font-black text-4xl text-center text-teal-900">Nasmer Fontanilla</h1>
             <p className="text-center">Aspring Developer In Singapore</p>
               <div>
                 <ul className="list-none">
                     <p className="font-quicksand font-bold text-center mt-[50px]">Home</p>
-                    <p className="font-quicksand font-bold text-center mt-[50px]">About</p>
-                    <p className="font-quicksand font-bold text-center mt-[50px]">Skills</p>
-                    <p className="font-quicksand font-bold text-center mt-[50px]">Experience</p>
-                    <p className="font-quicksand font-bold text-center mt-[50px]">Education</p>
-                    <p className="font-quicksand font-bold text-center mt-[50px]">Projects</p>
-                    <p className="font-quicksand font-bold text-center mt-[50px]">Contact</p>
+                    <p className="font-quicksand font-bold text-center mt-[30px]">About</p>
+                    <p className="font-quicksand font-bold text-center mt-[30px]">Skills</p>
+                    <p className="font-quicksand font-bold text-center mt-[30px]">Experience</p>
+                    <p className="font-quicksand font-bold text-center mt-[30px]">Education</p>
+                    <p className="font-quicksand font-bold text-center mt-[30px]">Projects</p>
+                    <p className="font-quicksand font-bold text-center mt-[30px]">Contact</p>
                 </ul>
               
               </div>
             </div>
             );
         };
+
 
         export default SideNav;
