c8664d486127657dbaef0b330798dd54ed194e64
SinHope <nasmerfontanilla@gmail.com>
2023-01-07 01:35:50 +0800

    added framer-motion. Trying to input into preload page

--- components/preload.js
+++ components/preload.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { motion } from "framer-motion";
 
 
 function MainPage() {
@@ -16,6 +17,21 @@ function MainPage() {
   if(!preloaded) {
     return (
       <div>
+      <motion.svg
+      className="border-solid border-2 border-violet-900 w-60 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 fixed center"
+      animate={{
+        scale: [1, 2, 2, 1, 1],
+        rotate: [0, 0, 180, 180, 0],
+        borderRadius: ["0%", "0%", "50%", "50%", "0%"]
+      }}
+      transition={{
+        duration: 2,
+        ease: "easeInOut",
+        times: [0, 0.2, 0.5, 0.8, 1],
+        repeat: Infinity,
+        repeatDelay: 1
+      }}
+    />
         <h1 className="text-center overline decoration-emerald-900 decoration-4 font-black text-[1000px] mt-[300px]">Welcome To My Page!</h1>
       </div>
     );
--- pages/workexperience.js
+++ pages/workexperience.js
@@ -4,7 +4,7 @@ import React from 'react';
 const WorkExperience = (props) => {
     return (
         <div className="mt-[50px] ml-[1px] grid grid-cols-2 gap-4 motion-safe:animate-fadeIn">
-            <ul className="cursor-pointer border-solid border-2 border-violet-900 rounded-full w-60 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 shadow-2xl motion-safe:animate-fadeIn">
+            <ul className="border-soliD cursor-pointer d border-2 border-violet-900 rounded-full w-60 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 shadow-2xl motion-safe:animate-fadeIn">
                 <li className="text-center ml-[0px] hover:text-lg font-sans font-extrabold text-lg mt-[20px] motion-safe:animate-fadeIn">Procurement Executive<p className="text-sm font-bold motion-safe:animate-fadeIn">(Current)(3+years) </p></li>
             </ul>
             <ul className="cursor-pointer border-solid border-2 border-violet-900 rounded-full w-60 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 shadow-2xl">
