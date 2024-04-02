a139ecb6280b77186d484d9ab1d6e64ff6c60323
Nasmer Fontanilla <nasmerfontanilla@gmail.com>
2024-04-02 12:00:49 +0800

    9th commit. solving issues and debugging.

--- src/lib/mdx.js
+++ src/lib/mdx.js
@@ -1,28 +1,30 @@
 import glob from 'fast-glob'
 
 async function loadEntries(directory, metaName) {
-  return (
-    await Promise.all(
-      (await glob('**/page.mdx', { cwd: `src/app/${directory}` })).map(
-        async (filename) => {
-          let metadata = (await import(`../app/${directory}/${filename}`))[
-            metaName
-          ]
-          return {
-            ...metadata,
-            metadata,
-            href: `/${directory}/${filename.replace(/\/page\.mdx$/, '')}`,
-          }
-        },
-      ),
-    )
-  ).sort((a, b) => b.date.localeCompare(a.date))
+  const entries = await Promise.all(
+    (await glob('**/page.mdx', { cwd: `src/app/${directory}` })).map(
+      async (filename) => {
+        let metadata = (await import(`../app/${directory}/${filename}`))[metaName];
+        return {
+          ...metadata,
+          metadata,
+          href: `/${directory}/${filename.replace(/\/page\.mdx$/, '')}`,
+        };
+      },
+    ),
+  );
+  
+  return entries.sort((a, b) => {
+    const dateA = a.date || "0000-00-00"; // Assign a minimum date if `date` is falsy
+    const dateB = b.date || "0000-00-00"; // This ensures comparison is always between strings
+    return dateB.localeCompare(dateA);
+  });
 }
 
 export function loadArticles() {
-  return loadEntries('blog', 'article')
+  return loadEntries('blog', 'article');
 }
 
 export function loadCaseStudies() {
-  return loadEntries('work', 'caseStudy')
+  return loadEntries('work', 'caseStudy');
 }
